f5a3e555308219e523ab0b669e437f71415d19c4
Poul Henriksen <pohe@easj.dk>
2021-10-05 14:16:44 +0200

    Add project files.

---
+++ BookCatalog/BookCatalogList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookCatalog
+{
+    public class BookCatalogList
+    {
+        #region Instance fields
+        private List<Book> _books;
+        #endregion
+
+        #region Constructor
+        public BookCatalogList()
+        {
+            _books = new List<Book>();
+        }
+        #endregion
+
+        #region Properties
+        public int Count
+        {
+            get { return _books.Count; }
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// This method adds a single Book object 
+        /// to the List of books 
+        /// </summary>
+        public void AddBook(Book aBook)
+        {
+
+            // TODO
+        }
+
+        /// <summary>
+        /// This method returns a Book object (if any) from
+        /// the List of books, which has a matching ISBN number.
+        /// If no such object exists, the method returns null.
+        /// </summary>
+        public Book LookupBook(string isbn)
+        {
+            // TODO
+            return null;
+        }
+
+        /// <summary>
+        /// This method deletes a Book object from the List
+        /// of books, specifically the object which has a
+        /// matching ISBN number. If no such object exists,
+        /// no object is deleted.
+        /// </summary>
+        public void DeleteBook(string isbn)
+        {
+
+            // TODO
+        }
+
+
+
+        #endregion
+    }
+}
---
+++ BookCatalog/InsertCodeHere.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BookCatalog
+{
+    class InsertCodeHere
+    {
+        // public int ShowMenu()
+        // {
+        //     //
+        // }
+
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            Console.WriteLine("Hello world!");
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+    
+}
